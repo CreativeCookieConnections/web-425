68d7cb90de7a11fb6b34ca9df6b419cdcc4bdb36
Aisha Keller <creativecookieconnections@gmail.com>
2026-03-26 06:59:50 -0500

    working on assignment

--- rpg-character-builder/src/app/app.component.ts
+++ rpg-character-builder/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <div class="wrapper">
+      <header class="banner">
+        <img src="/assets/RpgCharacterBuilder.png" alt="website banner for RPG Character Builder which portrays three figures dressed in medival attire."/>
+      </header>
+
+      <main class="main-content">
+
+      <nav class="navbar">
+      <ul>
+        <li><a routerLink="/">Home</a></li>
+        <li><a href="#">Character Creation</a></li>
+        <li><a href="#">Character Management</a></li>
+        <li><a href="#">Resources</a></li>
+        <li><a href="#">Community</a></li>
+      </ul>
+    </nav>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'rpg-character-builder';
