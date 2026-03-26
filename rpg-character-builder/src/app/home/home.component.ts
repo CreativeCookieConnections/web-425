cc2be03f51a57f0b5567583ca5e5c4dce8186afe
Aisha Keller <creativecookieconnections@gmail.com>
2026-03-26 08:32:17 -0500

    working on applying the route and implementing home component

---
+++ rpg-character-builder/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>
+      <h1> Macrocosm </h1>
+      <h2> A Quest Awaits</h2>
+      <p>
+       Join the Macrocosm, a realm of endless possibilities, personal choices, and limitless adventures. In this world, you control your destiny. Forge your own path, shape your character, and embark on a journey where every decision matters. Whether you seek glory, wisdom, or companionship, the Macrocosm is your canvas for storytelling and self-discovery. Are you ready to step into the unknown and create your own Legend?
+        </p>
+
+      <p> Will you accept this quest? </p>
+    </div>
+  `,
+  styles: ``
+})
+export class HomeComponent {
+
+}
