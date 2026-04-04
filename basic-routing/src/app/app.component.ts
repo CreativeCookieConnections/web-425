5c0775017dc7bc0000fa4929e5764405b25fbdff
Aisha Keller <creativecookieconnections@gmail.com>
2026-04-04 00:45:41 -0500

    working more on chapter reading and implementing routing

--- basic-routing/src/app/app.component.ts
+++ basic-routing/src/app/app.component.ts
@@ -1,13 +1,89 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink],
+  template: `
+  <div class="container">
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+    
+    <nav>
+      <ul>
+        <li><a routerLink="/contact">Contact Us</a></li>
+        <li><a routerLink="/about">About Us</a></li>
+        <li><a routerLink="/services">Our Services</a></li>
+      </ul>
+    </nav>
+
+    <main>
+      <router-outlet/>
+    </main>
+  
+    <footer>
+      &copy; {{ year }} Basic routing Example <br/>
+      Professor Krasso
+    </footer>
+  </div>
+  `,
+
+  styles: `
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    width: 75%;
+    margin: 0 auto;
+  }
+
+    header{
+      background-color: black;
+      color: white;
+      padding: 10px;
+      text-align: center;
+    }
+
+    main{
+      flex: 1;
+    }
+
+    nav{
+      background-color: #f8f8f8;
+      padding: 10px;
+      text-align: center;
+    }
+
+    li{
+      display: inline;
+      margin-right: 10px;
+      }
+
+    footer{
+      margin-top: 20px;
+      height: 50px;
+      padding: 10px;
+      background-color: black;
+      color: white;
+      text-align: center;
+    }
+  `
+
+
 })
 export class AppComponent {
-  title = 'basic-routing';
+  // variables to be used in teh template
+  // scope is limited to the class AppComponent
+  title: string;
+  year: number;
+
+  constructor() {
+    this.title="Basic Routing Example"; //assign the title property value of 'Basic Routing Exmaple'
+    this.year=new Date().getFullYear(); //assign the current year to the year property using the Date object
+  }
 }
+
+
--- basic-routing/src/app/app.routes.ts
+++ basic-routing/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { ViewServiceComponent } from './services/view-service/view-service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'services', pathMatch: 'full' },
+  { path: 'services', component: ServicesComponent },
+  { path: 'services/:id', component: ViewServiceComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'services' }
+];
